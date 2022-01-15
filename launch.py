5fe0ea062ffc0e3ee1536304b4810253f0fc9dff
TaisyoFranoria <taisyocbx400fs@gmail.com>
2022-01-16 04:07:07 +0900

    名称とか変更

--- launch.py
+++ launch.py
@@ -2,6 +2,14 @@ import subprocess as sp
 import random
 import os
 import Weather
+import datetime
+
+# test
+t_delta = datetime.timedelta(hours=9)
+JST = datetime.timezone(t_delta, 'JST')
+now = datetime.datetime.now(JST)
+print(now)
+#
 
 print('直近の天気を調べるね')
 order = 0
@@ -11,5 +19,5 @@ if(wd.getDate(0)=='None' or wd.getWeathers(order)=='None'):
     Say = '何もみえないよ。'
 #Say = '天気予報機能は現在メンテナンス中だよ'
 Num = str(random.randint(0,1))
-sp.Popen(['./ss.sh',Num,Say],cwd=os.path.dirname(__file__))
+sp.Popen(['./ShikimiShellArt.sh',Num,Say],cwd=os.getcwd())
 
