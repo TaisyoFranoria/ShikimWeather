d3be082b4c7476fc7cfec944128acb2d7bfcb8a1
TaisyoFranoria <taisyocbx400fs@gmail.com>
2022-02-13 21:43:46 +0900

    エリアコードについて色々考え中

---
+++ codeChecker/checker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export SCRIPT_DIR="$(cd $(dirname $0); pwd)"
+export WORK_DIR="${SCRIPT_DIR}/work"
+export DAYRY_DATAFILE=$(date "+%Y%m%d")
+
+cat codelist.dat | while read line
+do
+   echo "今日の天気を調べるね"
+   if [ -n ${1} ];then
+      python3 ${SCRIPT_DIR}/Outputer.py ${line} > ${WORK_DIR}/${DAYRY_DATAFILE}
+   else 
+      python3 ${SCRIPT_DIR}/Outputer.py "130000" > ${WORK_DIR}/${DAYRY_DATAFILE}
+   fi
+    
+   if [ $? -ne 0 ];then
+     echo $line >> err.dat
+   fi
+
+   ${SCRIPT_DIR}/../ShikimiShellArt.sh 0 $(cat ${WORK_DIR}/${DAYRY_DATAFILE})
+done
+
